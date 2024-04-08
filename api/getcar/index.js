9208ed6888dad148ec2dab4f6069c9811eae3dc5
PravirPadayachee <160501638+PravirPadayachee@users.noreply.github.com>
2024-04-08 16:01:39 +0000

    Update index.js

--- api/getcar/index.js
+++ api/getcar/index.js
@@ -1,6 +1,26 @@
-//create cars api using express
+const fs = require('fs');
+const path = require('path');
 
-const cars = require('/cars.json');
 module.exports = async function (context, req) {
-    context.res.json(cars);
+    try {
+        // Get the path to the cars.json file in the same directory
+        const filePath = path.resolve(__dirname, 'cars.json');
+        
+        // Read the contents of the file synchronously
+        const data = fs.readFileSync(filePath, 'utf8');
+        
+        // Parse the JSON data
+        const carsData = JSON.parse(data);
+        
+        // Return the contents of the cars.json file
+        context.res = {
+            status: 200,
+            body: carsData
+        };
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: error.message
+        };
+    }
 };
