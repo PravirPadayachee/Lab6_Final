5b7045d38a3ccb98f9bb89c670d1e4da82e3f33e
PravirPadayachee <160501638+PravirPadayachee@users.noreply.github.com>
2024-04-08 15:52:57 +0000

    Update index.js

--- api/getcar/index.js
+++ api/getcar/index.js
@@ -1,7 +1,4 @@
 //create cars api using express
-const express = require('express');
-const app = express();
-app.use(express.json());
 
 const cars = require('/cars.json');
 module.exports = async function (context, req) {
