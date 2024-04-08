1534936900516a2274762dd124ec971753b5236e
PravirPadayachee <160501638+PravirPadayachee@users.noreply.github.com>
2024-04-08 16:05:23 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
-   async function() {
-         await( await fetch(`/api/getcar`));
-    }();
+
+   (async function() {
+        try {
+            const carsResponse = await fetch('/api/getcar'); // Change the route to match your Azure Function
+            if (!carsResponse.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const carsData = await carsResponse.json();
+            console.log('Car Information:', carsData);
+        } catch (error) {
+            console.error('Error fetching car data:', error);
+        }
+    })();
+
